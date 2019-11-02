fe78327d4410f13c0a7f84fb0446d8b2c8b3d582
Bakti Wijaya <baktidwidharmawijaya@gmail.com>
2019-11-03 03:40:33 +0700

    Modul Transaksi Coin

---
+++ application/controllers/admin/Transaksipoin.php
@@ -0,0 +1,98 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class transaksipoin extends CI_Controller {
+
+    protected $user_type;
+
+#------------------------------------    
+# constructor function
+#------------------------------------    
+
+    public function __construct() {
+        parent::__construct();
+        $session_id = $this->session->userdata('session_id');
+        $user_type = $this->session->userdata('user_type');
+        if ($user_type != 1 || $session_id == NULL) {
+            redirect('authentication/keluar');
+        }
+    }
+
+    public function index() {
+        $data['title'] = 'Verifikasi Pendapatan Poin';
+        $data['content'] = 'admin/transaksipoin/index';
+        $this->load->view('admin/template/main', $data);
+    }
+
+    public function load_table() {
+        $data['list'] = $this->Crud_m->all_data('transaksi_poin', '*','status=1');
+        $this->load->view('admin/transaksipoin/table', $data);
+    }
+
+    public function update() {
+        extract($_POST);
+
+        $data = array(
+            'verified_by' => $this->session->userdata('id'),
+            'tanggal_verifikasi' => date('Y-m-d H:i:s'),
+            'status' => 2
+        );
+        $update = $this->Crud_m->edit('transaksi_poin', $data, 'id_transaksi', $id);
+        if ($update) {
+            $id_pembeli = $this->Global_m->getvalue('id_user','transaksi_poin','id_transaksi',$id);
+            $exist = $this->Global_m->isExists('id_user',$id_pembeli,'transaksi_koinpoin');
+
+            if($exist) {
+                $total_poin    = $this->Global_m->getvalue('total_poin','transaksi_koinpoin','id_user',$id_pembeli);
+                $id_paketpoin  = $this->Global_m->getvalue('id_paketpoin','transaksi_poin','id_transaksi',$id);
+                $jumlah_koin   = $this->Global_m->getvalue('jumlah_paketpoin','master_paketpoin','id_paketpoin',$id_paketpoin);
+
+                $data2 = array(
+                    'total_poin' => ($total_poin + $jumlah_koin)
+                );
+
+                $update_jumlah = $this->Crud_m->edit('transaksi_koinpoin', $data2, 'id_user', $id_pembeli);
+                if($update_jumlah) {
+                    $message = array(TRUE, 'Proses Berhasil !', 'Proses pengubahan data berhasil !');
+                } else {
+                    $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+                }
+            } else {
+                $id_paketpoin  = $this->Global_m->getvalue('id_paketpoin','transaksi_poin','id_transaksi',$id);
+                $jumlah_poin   = $this->Global_m->getvalue('jumlah_paketpoin','master_paketpoin','id_paketpoin',$id_paketpoin);
+
+                $data2 = array(
+                    'total_poin' => $jumlah_poin,
+                    'id_user' => $id_pembeli
+
+                );
+                $update_jumlah = $this->Crud_m->add('transaksi_koinpoin', $data2);
+                if($update_jumlah) {
+                    $message = array(TRUE, 'Proses Berhasil !', 'Proses pengubahan data berhasil !');
+                } else {
+                    $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+                }
+            }
+        } else {
+            $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+        }
+        echo json_encode($message);
+    }
+
+    public function delete() {
+        extract($_POST);
+
+        $data = array(
+            'status' => 4
+        );
+        $update = $this->Crud_m->edit('transaksi_poin', $data, 'id_transaksi', $id);
+        if ($update) {
+            $message = array(TRUE, 'Proses Berhasil !', 'Proses pengubahan data berhasil !');
+        } else {
+            $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+        }
+        echo json_encode($message);
+    }
+
+}
---
+++ application/views/admin/transaksipoin/table.php
@@ -0,0 +1,148 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        $("[rel=tooltip]").tooltip({placement: 'right'});
+    });
+</script>
+<table class="table" id="datatable">
+    <thead>
+        <tr>
+            <th style="text-align: center;width: 5%;">#</th>
+            <th style="width: 15%;text-align: center;">Nama Paket Coin</th>
+            <th style="width: 15%;text-align: center;">Jumlah Poin</th>
+            <th style="width: 15%;text-align: center;">User Pembeli</th>
+            <th style="width: 20%;text-align: center;">Tanggal Pembelian</th>
+            <th style="width: 20%;text-align: center;">Status</th>
+            <th style="width: 20%;text-align: center;">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $no = 1;
+        foreach ($list as $key) {
+            ?>
+            <tr>
+                <td style="text-align: center"><?= $no ?></td>
+                <td style="text-align: center"><?= $this->Global_m->getvalue('nama_paketpoin','master_paketpoin','id_paketpoin',$key['id_paketpoin']); ?></td>
+                <td style="text-align: center"><?= number_format($this->Global_m->getvalue('jumlah_paketpoin','master_paketpoin','id_paketpoin',$key['id_paketpoin']),'2',',','.'); ?></td>
+                <td style="text-align: center"><?php echo $this->Global_m->getvalue('nama_lengkap','user_info','id',$key['id_user']) ?></td>
+                <td style="text-align: center;"><?php echo ($key['tanggal_pembelian'] != '') ? $key['tanggal_pembelian'] : '-' ?></td>
+                <td style="text-align: center;">
+                    <?php 
+                        if($key['status'] == 0) {
+                            echo "Belum diproses";
+                        } else if($key['status'] == 1) {
+                            echo "Sedang Review";
+                        } else if($key['status'] == 3){
+                            echo "Sudah diverifikasi";
+                        } else {
+                            echo "Transaksi Dibatalkan";
+                        }
+                    ?>    
+                </td>
+                <td style="text-align: center;">
+                    <?php if($key['status'] == 0) { ?>
+                        <a href="#" class="btn btn-default" onclick="edit('<?= $key['id_transaksi'] ?>')"><i class="icon-upload"></i></a>
+                    <?php } else if($key['status'] == 1) { ?>
+                        <a href="#" class="btn btn-default" onclick="hapus('<?= $key['id_transaksi'] ?>','<?php echo base_url() ?>uploads/bukti_pembayaran/<?php echo $key['gambar'] ?>','<?php echo $key['note'] ?>')"><i class="icon-check"></i></a>
+                    <?php } ?>
+                    
+                </td> 
+            </tr>
+            <?php
+            $no++;
+        }
+        ?>
+    </tbody>
+</table> 
+<?= form_close(); ?>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#datatable').DataTable({
+            bSort: false,
+            bLengthChange: false,
+            "oLanguage": {
+                "sSearch": ""
+            }
+        })
+    })
+    function edit(id,gambar,note) {
+        $.ajax({
+            url: "<?= base_url() ?>admin/transaksipoin/edit",
+            type: "POST",
+            data: {
+                id: id
+            },
+            beforeSend: function (data) {
+                $.blockUI({
+                    message: '<i class="icon-spinner4 spinner"></i>',
+                    overlayCSS: {
+                        backgroundColor: '#1b2024',
+                        opacity: 0.8,
+                        zIndex: 1200,
+                        cursor: 'wait'
+                    },
+                    css: {
+                        border: 0,
+                        color: '#fff',
+                        zIndex: 1201,
+                        padding: 0,
+                        backgroundColor: 'transparent'
+                    }
+                });
+            },
+            error: function (data) {
+                $.unblockUI();
+                alert('Proses data gagal', 'info')
+            },
+            success: function (data) {
+                $.unblockUI();
+                $('#content').html(data);
+            }
+        });
+    }
+
+    function hapus(id,gambar,note) {
+        swalInit({
+            title: 'Bukti Share Gambar',
+            text: 'Verifikasi data ini ?',
+            imageUrl: gambar,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+            confirmButtonText: 'Verifikasi',
+            showCancelButton: true,
+            cancelButtonText: 'Kembali',
+        }).then(function (result) {
+            if (result.value) {
+                $.ajax({
+                    url: "<?= base_url() ?>admin/transaksipoin/update",
+                    type: "POST",
+                    data: {
+                        id: id
+                    },
+                    error: function (data) {
+                    },
+                    success: function (data) {
+                        var obj = JSON.parse(data);
+                        if (obj[0]) {
+                            swalInit({
+                                type: 'success',
+                                text: obj[2]
+                            }).then(function (con) {
+                                if (con.value) {
+                                    load()
+                                }
+                            })
+                        } else {
+                            swalInit({
+                                type: 'warning',
+                                text: obj[2]
+                            })
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+</script>
