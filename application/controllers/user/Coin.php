819296bb7483fe51e82e7b4c03791fdd3ed1182f
Bakti Wijaya <baktidwidharmawijaya@gmail.com>
2019-11-03 00:30:31 +0700

    Menu Transaksi Coin

---
+++ application/controllers/user/Coin.php
@@ -0,0 +1,110 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Coin extends CI_Controller {
+
+    protected $user_type;
+
+#------------------------------------    
+# constructor function
+#------------------------------------    
+
+    public function __construct() {
+        parent::__construct();
+        $session_id = $this->session->userdata('session_id');
+        $user_type = $this->session->userdata('user_type');
+        if ($user_type != 3 || $session_id == NULL) {
+            redirect('authentication/keluar');
+        }
+    }
+
+    public function index() {
+        $data['title'] = 'Pembelian Coin';
+        $data['content'] = 'user/coin/index';
+        $this->load->view('user/template/main', $data);
+    }
+
+    public function load_table() {
+        $data['list'] = $this->Crud_m->all_data('transaksi_coin', '*');
+        $this->load->view('user/coin/table', $data);
+    }
+
+    public function add() {
+        $data['coin'] = $this->Crud_m->all_data('master_paketcoin', '*');
+        $this->load->view('user/coin/add',$data);
+    }
+
+    public function edit() {
+        extract($_POST);
+        $data['id'] = $id;
+        $data['detail'] = $this->Crud_m->get_one('*', 'transaksi_coin', 'id_transaksi', $id);
+        $this->load->view('user/coin/edit', $data);
+    }
+
+    public function save() {
+        extract($_POST);
+        $data = array(
+            'id_paketcoin' => $id_paketcoin,
+            'tanggal_pembelian' => date('Y-m-d H:i:s'),
+            'id_user' => $this->session->userdata('id'),
+            'status' => 0
+        );
+
+        $add = $this->Crud_m->add('transaksi_coin', $data);
+        if ($add) {
+            $message = array(TRUE, 'Proses Berhasil !', 'Proses penyimpanan data berhasil !');
+        } else {
+            $message = array(FALSE, 'Proses Gagal !', 'Proses penyimpanan data gagal !');
+        }
+
+        echo json_encode($message);
+    }
+
+    public function update() {
+        extract($_POST);
+
+        $upload1 = $_FILES['gambar']['name'];
+        $nmfile1 = time()."_".$upload1;
+        $config['upload_path']          = './uploads/bukti_pembayaran';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['max_size']             = 1024 * 1;
+        $config['file_name']            = $nmfile1;
+
+        $this->load->library('upload', $config);
+        $data1 = $this->upload->data();
+        $this->upload->do_upload('gambar');
+
+        $image = $data1['file_name'];
+
+        $data = array(
+            'gambar' => $image,
+            'note' => $note,
+            'tanggal_upload' => date('Y-m-d H:i:s'),
+            'status' => 1
+        );
+        $update = $this->Crud_m->edit('transaksi_coin', $data, 'id_transaksi', $id);
+        if ($update) {
+            $message = array(TRUE, 'Proses Berhasil !', 'Proses pengubahan data berhasil !');
+        } else {
+            $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+        }
+        echo json_encode($message);
+    }
+
+    public function delete() {
+        extract($_POST);
+
+        $data = array(
+            'status' => 4
+        );
+        $update = $this->Crud_m->edit('transaksi_coin', $data, 'id_transaksi', $id);
+        if ($update) {
+            $message = array(TRUE, 'Proses Berhasil !', 'Proses pengubahan data berhasil !');
+        } else {
+            $message = array(FALSE, 'Proses Gagal !', 'Proses pengubahan data gagal !');
+        }
+        echo json_encode($message);
+    }
+
+}
---
+++ application/views/user/coin/add.php
@@ -0,0 +1,82 @@
+<div class="row">
+    <?php foreach ($coin as $key) : ?>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                <i class="icon-coins icon-2x text-success-400 border-success-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
+                    <h5 class="card-title"><?php echo $key['nama_paketcoin'] ?></h5>
+                    <p class="mb-3"><b>Jumlah Coin</b> : <?php echo number_format($key['jumlah_paketcoin']); ?></p>
+                    <p class="mb-3"><b>Harga Coin</b> : <?php echo number_format($key['harga_paketcoin'],2,',','.'); ?></p>
+                    <a href="#" class="btn bg-success-400" onclick="save('<?php echo $key['id_paketcoin'] ?>')">Beli</a>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+<?= form_close(); ?>
+<script type="text/javascript">
+
+    function save(id) {
+        swalInit({
+            title: 'Konfirmasi !',
+            text: 'Apakah anda yakin ingin membeli paket coin ini ?',
+            type: 'warning',
+            confirmButtonText: 'Ya !',
+            showCancelButton: true,
+            cancelButtonText: 'Tidak !',
+        }).then(function (result) {
+            if (result.value) {
+                $.ajax({
+                    type: 'POST',
+                    url: '<?php echo base_url() ?>user/coin/save',
+                    data: {
+                        id_paketcoin : id,
+                    },
+                    beforeSend: function (data) {
+                        $.blockUI({
+                            message: '<i class="icon-spinner4 spinner"></i>',
+                            overlayCSS: {
+                                backgroundColor: '#1b2024',
+                                opacity: 0.8,
+                                zIndex: 1200,
+                                cursor: 'wait'
+                            },
+                            css: {
+                                border: 0,
+                                color: '#fff',
+                                zIndex: 1201,
+                                padding: 0,
+                                backgroundColor: 'transparent'
+                            }
+                        });
+                    },
+                    error: function (data) {
+                        $.unblockUI();
+                        alert('Proses data gagal', 'info')
+                    },
+                    success: function (data) {
+                        $.unblockUI();
+                        var obj = JSON.parse(data);
+                        if (obj[0]) {
+                            swalInit({
+                                type: 'success',
+                                text: obj[2]
+                            }).then(function (con) {
+                                if (con.value) {
+                                    load();
+                                }
+                            })
+                        } else {
+                            swalInit({
+                                type: 'warning',
+                                text: obj[2]
+                            })
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+</script>
