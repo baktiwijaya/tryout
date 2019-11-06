b9bf43bf978fc45d75c1dd6ddddad5543b0d4771
cagakpespa <54283882+cagakpespa@users.noreply.github.com>
2019-11-07 01:54:18 +0700

    Fix Tryout

--- application/controllers/user/Beli_tryout.php
+++ application/controllers/user/Beli_tryout.php
@@ -26,7 +26,7 @@ class Beli_tryout extends CI_Controller {
     }
 
     public function load_table() {
-        $data['list'] = $this->Crud_m->all_data('transaksi_tryout', '*');
+        $data['list'] = $this->Crud_m->all_data('transaksi_tryout', '*',"id_user=".$this->session->userdata('id'));
         $this->load->view('user/beli_tryout/table', $data);
     }
 
