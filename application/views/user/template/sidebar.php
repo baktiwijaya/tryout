d9cc78260be056fefdca07ea5541af319a9814eb
Bakti Wijaya <baktidwidharmawijaya@gmail.com>
2019-11-16 22:22:53 +0700

    Update sidebar.php

--- application/views/user/template/sidebar.php
+++ application/views/user/template/sidebar.php
@@ -1,14 +1,5 @@
- <?php
-    $results = $this->db->select('*')->from("settings")->where('id', 14)->get()->row();
-    $website_logo = json_decode($results->details);
-    ?>
-    <?php $bu = base_url(); ?>
-    <?php $bu_img = base_url(); ?>
-    <?php $slug1 = $this->uri->segment(1); ?>
-    <?php $slug = $this->uri->segment(2); ?>
-    <?php $slug3 = $this->uri->segment(3); ?>
-    <?php $selected = 'active'; 
-?>
+<?php $bu = base_url(); ?>
+
 <div class="navbar navbar-default" id="navbar-second">
     <ul class="nav navbar-nav no-border visible-xs-block">
         <li><a class="text-center collapsed" data-toggle="collapse" data-target="#navbar-second-toggle"><i class="icon-menu7"></i></a></li>
@@ -16,32 +7,31 @@
 
     <div class="navbar-collapse collapse" id="navbar-second-toggle">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="<?php echo base_url() ?>user/dashboard"><i class="icon-display4 position-left"></i> Dashboard</a></li>
+            <li class="nav-item <?php if($menu == 'Dashboard'){echo "active";} ?>"><a href="<?php echo base_url() ?>user/dashboard"><i class="icon-display4 position-left"></i> Dashboard</a></li>
             <?php
                 $mutama = $this->Crud_m->all_data('menu', '*', "parent = 0 and role ='" . $this->session->userdata('user_type') . "' and is_aktif = 1", 'order asc');
-                foreach ($mutama as $value) :
-                    ?>
+                foreach ($mutama as $value) : ?>
                     <?php
                     $submenu = $this->Crud_m->all_data('menu', '*', "parent = '" . $value['id_menu'] . "' and role ='" . $this->session->userdata('user_type') . "' and is_aktif = 1", 'order asc');
                     $jum = $this->Crud_m->get('menu', 'count(*) as jumlah', "parent = '" . $value['id_menu'] . "' and role ='" . $this->session->userdata('user_type') . "' and is_aktif = 1", 1, NULL, TRUE);
                     if ($jum->jumlah > 0) {
                         ?>
                         <li class="dropdown">
-                            <a href="#" class="dropdown" data-toggle="dropdown">
+                            <a href="#" class="dropdown-item  <?php if($menu == $value['nama_menu']){echo "dropdown-item-open";} ?>" data-toggle="dropdown">
                                 <i class="icon-basket position-left"></i> <?php echo $value['nama_menu']; ?> <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu width-250">
                                 <?php foreach ($submenu as $key) : ?>
                                    <li class="dropdown">
-                                    <a href="<?php $bu ?><?php echo $key['target'] ?>"><i class="<?php echo $key['icon'] ?>"></i> <?php echo $key['nama_menu']; ?></a>
+                                    <a href="<?php $bu ?><?php echo $key['target'] ?>" class="dropdown-item <?php if($smenu == $key['nama_menu']){echo "active";} ?>"><i class="<?php echo $key['icon'] ?>"></i> <?php echo $key['nama_menu']; ?></a>
                                 </li>
                                 <?php endforeach; ?>
                                 
                             </ul>
                         </li>
                     <?php } else { ?>  
-                        <li class=""><a href="<?php $bu ?><?php echo $value['target'] ?>"><i class="<?php echo $value['icon'] ?> position-left"></i> <?php echo $value['nama_menu'] ?></a></li>
+                        <li class="nav-item <?php if($menu == $value['nama_menu']){echo "active";} ?>"><a href="<?php $bu ?><?php echo $value['target'] ?>"><i class="<?php echo $value['icon'] ?> position-left"></i> <?php echo $value['nama_menu'] ?></a></li>
                     <?php } ?>  
                 <?php endforeach; ?>
         </ul>
